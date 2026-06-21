3afa1b0dd8c76ccb51d4c2cb8e290ef61ebf35b8
laurinyev <retek1544@gmail.com>
2026-06-21 08:49:58 +0200

    Proper memory viewer that doesnt look like shit and lets you see the stuff going on on the B bus

--- src/UI/sdl3_GUI/mem_view.rs
+++ src/UI/sdl3_GUI/mem_view.rs
@@ -8,14 +8,22 @@ pub fn draw(ui: &Ui, state: &mut EmulatorState) {
         },
         crate::BackendState::NES(s) => {
             if ui.collapsing_header("CPU memory", TreeNodeFlags::empty()) {
-                ui.text("this is the CPU memory");
-                ui.child_window("rickroll");
-                
                 for i in 0..(0x10000/0x10) as usize{
-                    let mut string = format!("{:x} | ", i*0x10);
+                    let mut string = format!("{:04x} | ", i*0x10);
                     for j in 0..0x10 {
                         let val = s.bus.abus_read(((i*0x10)+j) as u16);
-                        string.push_str(format!("{val:x} ").as_str());
+                        string.push_str(format!("{val:02x} ").as_str());
+                    }
+                    string.push('\n');
+                    ui.text(string);
+                }
+            }
+            if ui.collapsing_header("PPU memory", TreeNodeFlags::empty()) {
+                for i in 0..(0x10000/0x10) as usize{
+                    let mut string = format!("{:04x} | ", i*0x10);
+                    for j in 0..0x10 {
+                        let val = s.bus.bbus_read(((i*0x11)+j) as u16);
+                        string.push_str(format!("{val:02x} ").as_str());
                     }
                     string.push('\n');
                     ui.text(string);
